1ec48cdc36ffa1bc53795e1d1f537abd6e558119
shanlialefkhani <shanli.alefkhani@gmail.com>
2023-01-08 17:06:29 +0330

    Refactor code

--- N-Queens-Project/hill-climbing.py
+++ N-Queens-Project/hill-climbing.py
@@ -1,35 +1,15 @@
-from random import randint
-from utils import calculate_objective, print_board
-
-
-def get_neighbour(state):
-	op_state = state.copy()
-	op_obj = calculate_objective(op_state)
-
-	for i in range(len(state)):
-		for j in range(len(state)):
-			if j != state[i]:
-				neighbour_state = state.copy()
-				neighbour_state[i] = j
-				neighbour_obj = calculate_objective(neighbour_state)
-
-				if neighbour_obj <= op_obj:
-					op_obj = neighbour_obj
-					op_state = neighbour_state.copy()
-	return op_state
+from utils import calculate_objective, get_neighbours, print_board, select_best_neighbours, get_random_state
 
 
 if __name__ == "__main__":
 	n = int(input())
-	state = [randint(0, n - 1) for _ in range(n)]
+	state = get_random_state(n)
 	print("\n"*(n + 1), end="")
 	while True:
-		next_state = get_neighbour(state.copy())
-
-		if state == next_state:
-			print_board(state)
-			break
-		elif calculate_objective(state) == calculate_objective(next_state):
-			next_state[randint(0, n - 1)] = randint(0, n - 1)
 		print_board(state)
+
+		next_state = select_best_neighbours(get_neighbours(state.copy()))[0]
+
+		if calculate_objective(state) <= calculate_objective(next_state):
+			break
 		state = next_state
--- N-Queens-Project/utils.py
+++ N-Queens-Project/utils.py
@@ -1,5 +1,6 @@
 import time
 from collections import Counter
+import random
 
 
 def board(i, j, state):
@@ -22,3 +23,36 @@ def print_board(state):
 	print("\033[F"*(len(state) + 1))
 	print("\n".join(" ".join("\u2655" if board(i, j, state) else "." for j in range(len(state)))for i in range(len(state))))
 	time.sleep(2)
+
+
+def get_neighbours(state=[]):
+    successors = []
+    n = len(state)
+
+    for column in range(n):
+        for row in range(n):
+            if state[column] != row:
+                successor = state.copy()
+                successor[column] = row
+                successors.append(successor)
+
+    return successors
+
+
+def select_best_neighbours(states, k=1):
+	candidates = []
+
+	for state in states:
+		state_cost = calculate_objective(state)
+		candidates.append([state, state_cost])
+
+	return [item[0] for item in sorted(candidates, key=lambda item: item[1])[:k]]
+
+
+def get_random_state(n):
+	return [random.randint(0, n - 1) for _ in range(n)]
+
+
+def get_random_neighbours(state, k=1):
+        next_states = get_neighbours(state)
+        return [random.choice(next_states) for _ in range(k)]
