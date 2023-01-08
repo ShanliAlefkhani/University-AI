0d84159cbe12983a5bee17e756b3bf928e783092
shanlialefkhani <shanli.alefkhani@gmail.com>
2023-01-08 17:06:57 +0330

    Add local beam search

---
+++ N-Queens-Project/local-beam-search.py
@@ -0,0 +1,21 @@
+import random
+
+from utils import get_neighbours, get_random_neighbours, get_random_state, print_board, calculate_objective, select_best_neighbours
+
+
+if __name__ == "__main__":
+    n = int(input())
+    k = int(input())
+    state = get_random_state(n)
+    print("\n"*(n + 1), end="")
+    k_states = get_random_neighbours(state, k)
+    while calculate_objective(state):
+        print_board(state)
+        next_state_canditates = []
+        for state in k_states:
+            next_state_canditates += get_neighbours(state)
+
+        k_states = select_best_neighbours(next_state_canditates, k)
+
+        state = select_best_neighbours(k_states)[0]
+    print_board(state)
