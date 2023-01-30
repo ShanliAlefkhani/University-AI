7bd8c1cfb759a75ae5e4403181b14b01525e2e70
shanlialefkhani <shanli.alefkhani@gmail.com>
2023-01-31 01:54:22 +0330

    refactor

--- N-Queens-Project/genetic.py
+++ N-Queens-Project/genetic.py
@@ -1,24 +1,24 @@
 import itertools
 import random
 from math import comb
-from utils import get_random_state, calculate_objective
+from utils import get_random_state, calculate_objective, print_board
 
-NUM_QUEENS = 8
-POPULATION_SIZE = 10
+
+POPULATION_SIZE = 100
 MIXING_NUMBER = 2
 MUTATION_RATE = 0.05
 
 
-def fitness_score(seq):
-    return 28 - calculate_objective(seq)
+def fitness_score(state):
+    return comb(len(state), 2) - calculate_objective(state)
 
 
-def selection(population):
-    return [ind for ind in population if random.randrange(comb(NUM_QUEENS, 2)*2) < fitness_score(ind)]
+def random_selection(population, n):
+    return [ind for ind in population if random.randrange(comb(n, 2)) < fitness_score(ind)]
 
 
-def crossover(parents):
-    cross_points = random.sample(range(NUM_QUEENS), MIXING_NUMBER - 1)
+def crossover(parents, n):
+    cross_points = random.sample(range(n), MIXING_NUMBER - 1)
     offsprings = []
     permutations = list(itertools.permutations(parents, MIXING_NUMBER))
     
@@ -39,26 +39,29 @@ def crossover(parents):
     return offsprings
 
 
-def mutate(seq):
-    for row in range(len(seq)):
+def mutate(state):
+    for row in range(len(state)):
         if random.random() < MUTATION_RATE:
-            seq[row] = random.randrange(NUM_QUEENS)
-    return seq
+            state[row] = random.randrange(len(state))
+    return state
 
 
-def print_found_goal(population):
-    for ind in population:
-        score = fitness_score(ind)
-        print(f'{ind}. Score: {score}')
-        if score == comb(NUM_QUEENS, 2):
+def found_goal(states):
+    for state in states:
+        if calculate_objective(state) == 0:
             return True
     return False
 
 
-population = [get_random_state(NUM_QUEENS) for _ in range(POPULATION_SIZE)]
-    
-while not print_found_goal(population):
-    parents = selection(population)
-    offsprings = list(map(mutate, crossover(parents)))
-    new_gen = offsprings + population
-    population =  sorted(new_gen, key=lambda ind: fitness_score(ind), reverse=True)[:POPULATION_SIZE]
+def genetic(n):
+    population = [get_random_state(n) for _ in range(POPULATION_SIZE)]
+    print("\n"*(n + 1), end="")
+        
+    while not found_goal(population):
+        print_board(sorted(population, key=lambda ind: fitness_score(ind), reverse=True)[0])
+        parents = random_selection(population, n)
+        offsprings = list(map(mutate, crossover(parents, n)))
+        new_gen = offsprings + population
+        population =  sorted(new_gen, key=lambda ind: fitness_score(ind), reverse=True)[:POPULATION_SIZE]
+
+    print_board(sorted(population, key=lambda ind: fitness_score(ind), reverse=True)[0])
