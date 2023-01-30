73d807e4cf1a427c64f7f3fe921db4f03129040c
shanlialefkhani <shanli.alefkhani@gmail.com>
2023-01-31 01:32:54 +0330

    refactor code

--- N-Queens-Project/hill_climbing.py
+++ N-Queens-Project/hill_climbing.py
@@ -1,13 +1,12 @@
 from utils import calculate_objective, get_neighbours, print_board, select_best_neighbours, get_random_state
 
 
-if __name__ == "__main__":
-	n = int(input())
+def hill_climbing(n):
 	state = get_random_state(n)
 	print("\n"*(n + 1), end="")
+
 	while True:
 		print_board(state)
-
 		next_state = select_best_neighbours(get_neighbours(state.copy()))[0]
 
 		if calculate_objective(state) <= calculate_objective(next_state):
---
+++ N-Queens-Project/simulated_annealing.py
@@ -0,0 +1,24 @@
+import math
+import decimal
+import random
+
+from utils import get_random_neighbours, get_random_state, print_board, calculate_objective
+
+
+def simulated_annealing(n):
+    temperature = 4000
+    schedule = 0.99
+    state = get_random_state(n)
+    print("\n"*(n + 1), end="")
+
+    while calculate_objective(state) or temperature == 0:
+        print_board(state)
+        temperature *= schedule
+        next_state = get_random_neighbours(state.copy())[0]
+        e = calculate_objective(state) - calculate_objective(next_state)
+        exp = decimal.Decimal(decimal.Decimal(math.e) ** (decimal.Decimal(e) * decimal.Decimal(temperature)))
+
+        if e > 0 or random.uniform(0, 1) < exp:
+            state = next_state
+
+    print_board(state)
